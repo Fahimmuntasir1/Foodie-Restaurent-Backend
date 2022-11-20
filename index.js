14237a49d541f4b7298f6786114ebc8dd12a7cd8
Fahimmuntasir1 <fahimmuntasir043@gmail.com>
2022-11-20 22:13:57 +0600

    new database created and data loded from mongodb and a new get api created

--- index.js
+++ index.js
@@ -3,10 +3,37 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // use middleware
 app.use(cors());
 app.use(express.json());
+
+// mongodb setup
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mvsti6m.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    await client.connect();
+    const foodCollection = client.db("FoodieRestaurant").collection("food");
+
+    // calling APIS
+    app.get("/foods", async (req, res) => {
+      const query = {};
+      const cursor = foodCollection.find(query);
+      const foods = await cursor.toArray();
+      res.send(foods);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 // root API calling
 app.get("/", (req, res) => {
